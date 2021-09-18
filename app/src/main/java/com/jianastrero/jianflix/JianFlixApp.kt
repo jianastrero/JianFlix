da0ab1c2a914d25a56c53674e3592b7449d1a862
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-18 23:29:49 +0800

    create JianFlixApp.kt and initialize logger

---
+++ app/src/main/java/com/jianastrero/jianflix/JianFlixApp.kt
@@ -0,0 +1,25 @@
+package com.jianastrero.jianflix
+
+import android.app.Application
+import com.jianastrero.core.util.Logger
+import com.jianastrero.jianflix.di.appModule
+import org.koin.android.ext.android.get
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class JianFlixApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@JianFlixApp)
+            modules(appModule)
+        }
+
+        // Initialize Logger
+        val logger = get<Logger>()
+        logger.init()
+    }
+}
