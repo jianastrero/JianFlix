fbd637fa08d9dac4995a687d7f56035fd53772c6
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 01:02:53 +0800

    add documentation logs to MovieRepository.kt

--- movie/movie_domain/src/main/java/com/jianastrero/movie_domain/MovieRepository.kt
+++ movie/movie_domain/src/main/java/com/jianastrero/movie_domain/MovieRepository.kt
@@ -4,8 +4,20 @@ import com.jianastrero.movie_domain.model.Movie
 
 interface MovieRepository {
 
+    /**
+     * Get All Movie Domains
+     *
+     * @author Jian James P. Astrero
+     */
     suspend fun getAll(): List<Movie>
 
+    /**
+     * Get Movie Domain by ID
+     *
+     * @param id The id of the Movie to be fetched
+     *
+     * @author Jian James P. Astrero
+     */
     suspend fun getById(id: Int): Movie
 
 }
