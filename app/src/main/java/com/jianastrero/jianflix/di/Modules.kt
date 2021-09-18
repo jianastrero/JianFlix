b8d347d656354b7ab521129e8934e0128223ce4c
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 01:47:03 +0800

    add loggin for debug mode in okhttp client

--- app/src/main/java/com/jianastrero/jianflix/di/Modules.kt
+++ app/src/main/java/com/jianastrero/jianflix/di/Modules.kt
@@ -2,16 +2,19 @@ package com.jianastrero.jianflix.di
 
 import android.app.Application
 import androidx.room.Room
+import com.jianastrero.constant.NetworkConstants
 import com.jianastrero.core.util.Logger
 import com.jianastrero.jianflix.BuildConfig
 import com.jianastrero.jianflix.JianFlixDatabase
 import com.jianastrero.movie_data.local.MovieDao
 import com.jianastrero.movie_data.remote.ITunesApi
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * App Module Instance for dependency injection
@@ -55,10 +58,22 @@ val networkModule = module {
 
     fun provideOkhttpClient(): OkHttpClient =
         OkHttpClient.Builder()
+            .connectTimeout(NetworkConstants.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(NetworkConstants.READ_TIMEOUT, TimeUnit.SECONDS)
+            .also { builder ->
+                if (BuildConfig.DEBUG) {
+                    builder.addInterceptor(
+                        HttpLoggingInterceptor().apply {
+                            level = HttpLoggingInterceptor.Level.BODY
+                        }
+                    )
+                }
+            }
             .build()
 
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
+            .baseUrl(NetworkConstants.BASE_URL)
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
