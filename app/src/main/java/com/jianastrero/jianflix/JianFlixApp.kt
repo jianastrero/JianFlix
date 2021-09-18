887e39d3ab67f985043d1f4017279319338ea813
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 00:13:17 +0800

    add comments for JianFlixApp.kt

--- app/src/main/java/com/jianastrero/jianflix/JianFlixApp.kt
+++ app/src/main/java/com/jianastrero/jianflix/JianFlixApp.kt
@@ -8,6 +8,11 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
+/**
+ * The Application that JianFlix will use
+ *
+ * @author Jian James P. Astrero
+ */
 class JianFlixApp : Application() {
 
     override fun onCreate() {
