fadf342b2b098ff2fd89d13addc999169765cbb0
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 00:21:10 +0800

    add documentation comments for MovieDao.kt

--- movie/movie_data/src/main/java/com/jianastrero/movie_data/local/MovieDao.kt
+++ movie/movie_data/src/main/java/com/jianastrero/movie_data/local/MovieDao.kt
@@ -5,18 +5,47 @@ import androidx.room.Insert
 import androidx.room.Query
 import com.jianastrero.movie_data.local.model.Movie
 
+/**
+ * Local Database Transactions for Movies
+ *
+ * @author Jian James P. Astrero
+ */
 @Dao
 interface MovieDao {
 
+    /**
+     * Get All Movies
+     *
+     * @author Jian James P. Astrero
+     */
     @Query("SELECT * FROM movies")
     suspend fun getAll(): List<Movie>
 
+    /**
+     * Get a Movie using its id
+     *
+     * @param id The id of the movie
+     *
+     * @author Jian James P. Astrero
+     */
     @Query("SELECT * FROM movies WHERE id = :id LIMIT 1")
-    suspend fun getById(id: String): Movie
+    suspend fun getById(id: Int): Movie
 
+    /**
+     * Insert List of Movies
+     *
+     * @param movies List of Movies to insert to local database
+     *
+     * @author Jian James P. Astrero
+     */
     @Insert
     suspend fun insertAll(movies: List<Movie>)
 
+    /**
+     * Delete all Movies
+     *
+     * @author Jian James P. Astrero
+     */
     @Query("DELETE FROM movies")
     suspend fun clear()
 
