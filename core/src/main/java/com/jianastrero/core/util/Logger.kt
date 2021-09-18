a346ae9e0dfc57f82784fd0cd6936bd4240b5b6c
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 00:04:59 +0800

    add documentation to Logger.kt

--- core/src/main/java/com/jianastrero/core/util/Logger.kt
+++ core/src/main/java/com/jianastrero/core/util/Logger.kt
@@ -1,14 +1,30 @@
 package com.jianastrero.core.util
 
+/**
+ * Logger class to use for loggin to logcat or somewhere else like crashlytics
+ *
+ * @param isDebug If true, log through the logcat. else, log somewhere else
+ * @param defaultTag The default tag for logs
+ * @constructor Creates an Instance of Logger
+ */
 class Logger(
     private val isDebug: Boolean,
     private val defaultTag: String
 ) {
 
+    /**
+     * Initialize Logger
+     */
     fun init() {
         INSTANCE = this
     }
 
+    /**
+     * Log a message with a tag to the Logger
+     *
+     * @param tag The tag to use for this log message. Default: defaultTag provided on initialization
+     * @param message The message for this log.
+     */
     fun log(tag: String = defaultTag, message: String) {
         if (isDebug) {
             println("$tag: $message")
