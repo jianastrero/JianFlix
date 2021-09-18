f57ad0034aba2b58bba76984c20d59753ccfda96
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 00:12:21 +0800

    add comments for app module

--- app/src/main/java/com/jianastrero/jianflix/di/AppModule.kt
+++ app/src/main/java/com/jianastrero/jianflix/di/AppModule.kt
@@ -4,6 +4,11 @@ import com.jianastrero.core.util.Logger
 import com.jianastrero.jianflix.BuildConfig
 import org.koin.dsl.module
 
+/**
+ * App Module Instance for dependency injection on the whole app
+ *
+ * @author Jian James P. Astrero
+ */
 val appModule = module {
     single { Logger(BuildConfig.DEBUG, "JianDebug") }
 }
