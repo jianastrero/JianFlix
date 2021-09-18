4088b99d85de5f76adb25992cc9995f6373d6587
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 01:45:42 +0800

    create NetworkConstants.kt

---
+++ constant/src/main/java/com/jianastrero/constant/NetworkConstants.kt
@@ -0,0 +1,9 @@
+package com.jianastrero.constant
+
+object NetworkConstants {
+
+    const val BASE_URL = "https://itunes.apple.com/"
+    const val CONNECT_TIMEOUT = 20L // Seconds
+    const val READ_TIMEOUT = 20L // Seconds
+
+}
