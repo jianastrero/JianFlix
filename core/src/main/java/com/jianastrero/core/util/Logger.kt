f72eacab07fed5392d3bd206153eebac06e16609
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 00:11:21 +0800

    additional documentation to Logger.kt

--- core/src/main/java/com/jianastrero/core/util/Logger.kt
+++ core/src/main/java/com/jianastrero/core/util/Logger.kt
@@ -3,6 +3,7 @@ package com.jianastrero.core.util
 /**
  * Logger class to use for loggin to logcat or somewhere else like crashlytics
  *
+ * @author Jian James P. Astrero
  * @param isDebug If true, log through the logcat. else, log somewhere else
  * @param defaultTag The default tag for logs
  * @constructor Creates an Instance of Logger
@@ -14,6 +15,8 @@ class Logger(
 
     /**
      * Initialize Logger
+     *
+     * @author Jian James P. Astrero
      */
     fun init() {
         INSTANCE = this
@@ -22,6 +25,7 @@ class Logger(
     /**
      * Log a message with a tag to the Logger
      *
+     * @author Jian James P. Astrero
      * @param tag The tag to use for this log message. Default: defaultTag provided on initialization
      * @param message The message for this log.
      */
@@ -38,6 +42,16 @@ class Logger(
     }
 }
 
+/**
+ * Log a message to the logger using the defaultTag
+ *
+ * @author Jian James P. Astrero
+ */
 fun String.log() = Logger.INSTANCE.log(message = this)
 
+/**
+ * Log a message to the logger using the given tag
+ *
+ * @author Jian James P. Astrero
+ */
 fun String.log(tag: String) = Logger.INSTANCE.log(tag, this)
