686da99045a70f80b9e43515b1d249e4098d09d3
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 16:41:46 +0800

    use GradientLetterBox for Trending Movie Composable

--- movie/movie_list/src/main/java/com/jianastrero/movie_list/TrendingMovie.kt
+++ movie/movie_list/src/main/java/com/jianastrero/movie_list/TrendingMovie.kt
@@ -2,41 +2,51 @@ package com.jianastrero.movie_list
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.jianastrero.common_ui.GradientLetterBox
 import com.jianastrero.core.util.iTunesArtworkUrlResize
 import com.jianastrero.movie_domain.model.Movie
+import com.jianastrero.movie_list.ui.PrimaryDark
 
 @ExperimentalCoilApi
 @Composable
-fun FeatureMovie(
+fun TrendingMovie(
     movie: Movie
 ) {
     var size by remember { mutableStateOf(IntSize.Zero) }
-    Column(
+    Box(
+        contentAlignment = Alignment.BottomCenter,
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        Box(
+        GradientLetterBox(
+            color = PrimaryDark,
+            contentAlignment = Alignment.BottomCenter,
+            height = size.height,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(240.dp)
+                .height(320.dp)
                 .onSizeChanged {
                     size = it
                 }
         ) {
-            val painter = coil.compose.rememberImagePainter(
+            val painter = rememberImagePainter(
                 data = movie.artwork.iTunesArtworkUrlResize((size.width / 1.6).toInt()),
                 builder = {
 
                 }
             )
             Image(
-                alignment = androidx.compose.ui.Alignment.Companion.Center,
+                alignment = Alignment.Center,
                 contentDescription = "Feature Movie",
                 contentScale = androidx.compose.ui.layout.ContentScale.Crop,
                 modifier = Modifier
@@ -45,5 +55,9 @@ fun FeatureMovie(
                 painter = painter,
             )
         }
+        Text(
+            text = movie.name,
+            color = Color.White
+        )
     }
 }
