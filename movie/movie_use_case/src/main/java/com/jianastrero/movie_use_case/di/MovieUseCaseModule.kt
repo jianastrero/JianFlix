05a3dedef547069dde09e12d6c46e4e4afd1445b
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-20 01:39:53 +0800

    add GetLatestMovieUseCase to movieUseCaseModule

--- movie/movie_use_case/src/main/java/com/jianastrero/movie_use_case/di/MovieUseCaseModule.kt
+++ movie/movie_use_case/src/main/java/com/jianastrero/movie_use_case/di/MovieUseCaseModule.kt
@@ -1,5 +1,6 @@
 package com.jianastrero.movie_use_case.di
 
+import com.jianastrero.movie_use_case.GetLatestMovieUseCase
 import com.jianastrero.movie_use_case.GetMovieUseCase
 import com.jianastrero.movie_use_case.GetMoviesCategorizedByGenreUseCase
 import com.jianastrero.movie_use_case.GetMoviesUseCase
@@ -10,5 +11,6 @@ val movieUseCaseModule = module {
     factory { GetMoviesUseCase() }
     factory { GetMovieUseCase() }
     factory { GetMoviesCategorizedByGenreUseCase() }
+    factory { GetLatestMovieUseCase() }
 
 }
