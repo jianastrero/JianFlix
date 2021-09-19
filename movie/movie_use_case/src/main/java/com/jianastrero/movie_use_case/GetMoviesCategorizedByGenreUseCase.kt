95e13c296e98fe8e60f4328d0d7c14b279839f1c
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 18:10:24 +0800

    create GetMoviesCategorizedByGenreUseCase.kt

---
+++ movie/movie_use_case/src/main/java/com/jianastrero/movie_use_case/GetMoviesCategorizedByGenreUseCase.kt
@@ -0,0 +1,50 @@
+package com.jianastrero.movie_use_case
+
+import com.jianastrero.core.domain.Resource
+import com.jianastrero.movie_domain.MovieRepository
+import com.jianastrero.movie_domain.model.Movie
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import retrofit2.HttpException
+import java.io.IOException
+import java.lang.Exception
+
+/**
+ * Use-case for Getting a single movie
+ *
+ * @author Jian James P. Astrero
+ */
+class GetMoviesCategorizedByGenreUseCase : KoinComponent {
+
+    private val repository by inject<MovieRepository>()
+
+    /**
+     * Invokable - object could invoke itself to call this method
+     */
+    operator fun invoke(id: Int): Flow<Resource<Map<String, List<Movie>>>> = flow {
+        try {
+            emit(Resource.Loading<Map<String, List<Movie>>>())
+
+            val movies = repository.getAll()
+            val genreList = movies.distinctBy { it.genre }.map { it.genre }
+
+            val genreMap = mutableMapOf<String, List<Movie>>()
+
+            genreList.forEach { genre ->
+                val genreMovies = movies.filter { it.genre == genre }
+                genreMap[genre] = genreMovies
+            }
+
+            emit(Resource.Success(genreMap as Map<String, List<Movie>>))
+
+        } catch (e: HttpException) {
+            emit(Resource.Error.UnexpectedError<Map<String, List<Movie>>>())
+        } catch (e: IOException) {
+            emit(Resource.Error.NoInternetConnectionError<Map<String, List<Movie>>>())
+        } catch (e: Exception) {
+            emit(Resource.Error<Map<String, List<Movie>>>(e.localizedMessage ?: "Unexpected error occurred"))
+        }
+    }
+}
