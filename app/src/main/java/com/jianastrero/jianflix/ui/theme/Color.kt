8b08c1d151ea8eab6c142edf34e112a34e4cc7d8
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 15:05:25 +0800

    update themes and colors

--- app/src/main/java/com/jianastrero/jianflix/ui/theme/Color.kt
+++ app/src/main/java/com/jianastrero/jianflix/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.jianastrero.jianflix.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightPurple = Color(0xFF590668)
+val Purple = Color(0xFF36054B)
+val DarkPurple = Color(0xFF200641)
+val Magenta500 = Color(0xFFE91E63)
