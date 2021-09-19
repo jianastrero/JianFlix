bd674b96e59aa162e77341df9ea71757879e3764
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-20 03:12:37 +0800

    add documentation

--- common_ui/src/main/java/com/jianastrero/common_ui/Screen.kt
+++ common_ui/src/main/java/com/jianastrero/common_ui/Screen.kt
@@ -1,5 +1,12 @@
 package com.jianastrero.common_ui
 
+/**
+ * Create Screen for all screens for easier navigation
+ *
+ * @param route The route for the screen
+ *
+ * @author Jian James P. Astrero
+ */
 sealed class Screen(val route: String) {
 
     object MovieMainListScreen : Screen("movie_main_list")
