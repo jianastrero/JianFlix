928b1919032358a84e4a89d097a8c57e66d23dd0
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 17:59:11 +0800

    add documentation to DateTime.kt

--- core/src/main/java/com/jianastrero/core/util/DateTime.kt
+++ core/src/main/java/com/jianastrero/core/util/DateTime.kt
@@ -3,6 +3,12 @@ package com.jianastrero.core.util
 import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
+/**
+ * Convert String to LocalDateTime
+ *
+ *
+ * @author Jian James P. Astrero
+ */
 fun String.toLocalDateTime(): LocalDateTime =
     ZonedDateTime.parse(replace("Z$".toRegex(), "+00:00"))
         .toLocalDateTime()
