dc21d990147006f4d6ee61b2c64b6835df6b4d29
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-20 02:06:50 +0800

    call getLatestMovieUseCase and call use-cases sequentially to not ruin state

--- movie/movie_main_list/src/main/java/com/jianastrero/movie_main_list/MovieMainListViewModel.kt
+++ movie/movie_main_list/src/main/java/com/jianastrero/movie_main_list/MovieMainListViewModel.kt
@@ -6,11 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jianastrero.core.domain.ProgressState
 import com.jianastrero.core.domain.Resource
+import com.jianastrero.movie_use_case.GetLatestMovieUseCase
 import com.jianastrero.movie_use_case.GetMoviesCategorizedByGenreUseCase
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
 class MovieMainListViewModel(
+    private val getLatestMovieUseCase: GetLatestMovieUseCase,
     private val getMoviesCategorizedByGenreUseCase: GetMoviesCategorizedByGenreUseCase
 ) : ViewModel() {
 
@@ -18,25 +20,49 @@ class MovieMainListViewModel(
     val state: State<MovieMainListState> = _state
 
     init {
-        getMoviesCategorizedByGenre()
+        getLatestMovie()
+    }
+
+    private fun getLatestMovie() {
+        getLatestMovieUseCase().onEach { resource ->
+            when (resource) {
+                is Resource.Success -> {
+                    _state.value = _state.value.copy(
+                        latestMovie = resource.data
+                    )
+                }
+                is Resource.Loading -> {
+                    _state.value = _state.value.copy(
+                        latestMovie = null
+                    )
+                }
+                is Resource.Error -> {
+                    _state.value = _state.value.copy(
+                        latestMovie = null
+                    )
+                }
+            }
+        }.launchIn(viewModelScope).invokeOnCompletion {
+            getMoviesCategorizedByGenre()
+        }
     }
 
     private fun getMoviesCategorizedByGenre() {
         getMoviesCategorizedByGenreUseCase().onEach { resource ->
             when (resource) {
                 is Resource.Success -> {
-                    _state.value = MovieMainListState(
+                    _state.value = _state.value.copy(
                         progressState = ProgressState.Loaded,
                         moviesCategorizedByGenre = resource.data ?: mapOf()
                     )
                 }
                 is Resource.Loading -> {
-                    _state.value = MovieMainListState(
+                    _state.value = _state.value.copy(
                         progressState = ProgressState.Loading
                     )
                 }
                 is Resource.Error -> {
-                    _state.value = MovieMainListState(
+                    _state.value = _state.value.copy(
                         progressState = ProgressState.Error(
                             resource.message ?: "An unexpected error had occured"
                         )
