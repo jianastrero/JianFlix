e0c3c22d8b60b80da7e6a68d659bbc557f67ad8d
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 17:58:30 +0800

    add documentation to currency extension function

--- core/src/main/java/com/jianastrero/core/util/Currency.kt
+++ core/src/main/java/com/jianastrero/core/util/Currency.kt
@@ -1,5 +1,12 @@
 package com.jianastrero.core.util
 
+/**
+ * Float Extension function to convert it to a String Currency
+ *
+ * @param currency The shown currency in the string. Default: None
+ *
+ * @author Jian James P. Astrero
+ */
 fun Float.toCurrency(currency: String = "") = "%.2f".format(this).let {
     if (currency.isEmpty()) {
         it
