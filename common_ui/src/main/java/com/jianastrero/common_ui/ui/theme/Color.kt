4b9e279661a956917c984c13a1fc925262e2060d
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 17:43:26 +0800

    move theme to common ui module

---
+++ common_ui/src/main/java/com/jianastrero/common_ui/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.jianastrero.jianflix.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryLight = Color(0xFF000000)
+val Primary = Color(0xFF000000)
+val PrimaryDark = Color(0xFF000000)
+val Magenta500 = Color(0xFFE91E63)
--- core/src/main/java/com/jianastrero/core/ui/theme/Color.kt
+++
@@ -1,6 +0,0 @@
-package com.jianastrero.core.ui
-
-val PrimaryLightHex = 0xFF000000
-val PrimaryHex = 0xFF000000
-val PrimaryDarkHex = 0xFF000000
-val Magenta500Hex = 0xFFE91E63
