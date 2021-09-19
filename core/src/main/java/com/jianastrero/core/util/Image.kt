32fc5bfc1c333b35d638c5304cbc763c9ddb0693
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-19 18:00:15 +0800

    add documentation to Image.kt

--- core/src/main/java/com/jianastrero/core/util/Image.kt
+++ core/src/main/java/com/jianastrero/core/util/Image.kt
@@ -2,6 +2,13 @@ package com.jianastrero.core.util
 
 import java.io.File
 
+/**
+ * Generate a new URL to fetch a new size of the iTunes Artwork URL
+ *
+ * @param newSize The new size of the image. The image is always square.
+ *
+ * @author Jian James P. Astrero
+ */
 fun String.iTunesArtworkUrlResize(newSize: Int): String {
     val file = File(this)
     val directory = file.parent
