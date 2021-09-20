c6150886bab935e8885c5901bc7d758fdef2bec6
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-20 10:00:35 +0800

    create CACHE_TIMEOUT and set to 1 minute

--- constant/src/main/java/com/jianastrero/constant/NetworkConstants.kt
+++ constant/src/main/java/com/jianastrero/constant/NetworkConstants.kt
@@ -5,5 +5,6 @@ object NetworkConstants {
     const val BASE_URL = "https://itunes.apple.com/"
     const val CONNECT_TIMEOUT = 20L // Seconds
     const val READ_TIMEOUT = 20L // Seconds
+    const val CACHE_TIMEOUT = 1 * 60 * 1000L // 1 Minute, 60 Seconds, 1000 Millisecond
 
 }
