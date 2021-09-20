8bfbadee45cb07524e4a842311dc497ec3270769
Jian James Astrero <jianjamesastrero@gmail.com>
2021-09-20 11:46:36 +0800

    create converter from millis toReadableHoursMinutesAndSeconds

--- core/src/main/java/com/jianastrero/core/util/DateTime.kt
+++ core/src/main/java/com/jianastrero/core/util/DateTime.kt
@@ -12,3 +12,11 @@ import java.time.ZonedDateTime
 fun String.toLocalDateTime(): LocalDateTime =
     ZonedDateTime.parse(replace("Z$".toRegex(), "+00:00"))
         .toLocalDateTime()
+
+fun Long.toReadableHoursMinutesAndSeconds(): String {
+    val seconds = this / 1000
+    val s = seconds % 60
+    val m = seconds / 60 % 60
+    val h = seconds / (60 * 60) % 24
+    return String.format("%d hours, %02d minutes %02d seconds", h, m, s)
+}
